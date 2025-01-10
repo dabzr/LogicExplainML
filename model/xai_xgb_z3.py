2b59d5bc7132a56b0b4c1694c0b006765a99a43a
viniciuspxc <viniciuspxc@gmail.com>
2025-01-10 20:19:12 -0300

    xgboost explainer

--- model/xai_xgb_z3.py
+++ model/xai_xgb_z3.py
@@ -1,17 +1,30 @@
 from z3 import *
-import numpy as np
 
 
-class Explainer:
-    def __init__(self, model, data):
+class XGBoostExplainer:
+    """Apenas classificação binária e base_score = None
+    data = X. labels = y
+    """
+
+    def __init__(self, model, data, labels):
+        """_summary_
+
+        Args:
+            model (XGBoost): xgboost model fited
+            data (DataFrame): dataframe (X or X_train)
+            labels (array): y (targets)
+        """
         self.model = model
-        self.data = data
+        self.data = data.values
+        self.columns = data.columns
         self.T_constraints = self.feature_constraints_expression(self.data)
         self.T_model = self.model_trees_expression(self.model)
         self.T = And(self.T_model, self.T_constraints)
+        self.label_proportions = labels.mean()
 
     def explain(self, instance, reorder="asc"):
-        self.D = self.decision_function_expression(self.model, [instance])
+        self.D = self.decision_function_expression(
+            self.model, [instance], self.label_proportions)
         self.I = self.instance_expression(instance)
 
         return self.explain_expression(self.I, self.T, self.D, self.model, reorder)
@@ -23,7 +36,7 @@ class Explainer:
             feature_values = X[:, i]
             min_val, max_val = feature_values.min(), feature_values.max()
 
-            x = Real(f"x{i}")
+            x = Real(self.columns[i])
             min = RealVal(min_val)
             max = RealVal(max_val)
 
@@ -33,12 +46,65 @@ class Explainer:
         return And(*constraints)
 
     def model_trees_expression(self, model):
-        formulas = []
-        for i, estimators in enumerate(model.estimators_):
-            for class_index, estimator in enumerate(estimators):
-                formula = self.tree_paths_expression(estimator, i, class_index)
-                formulas.append(formula)
-        return And(*formulas)
+        """
+        Constrói expressões lógicas para todas as árvores de decisão em um dataframe de XGBoost.
+        Para árvores que são apenas folhas, gera diretamente um And com o valor da folha.
+
+        Args:
+            df (pd.DataFrame): Dataframe contendo informações das árvores.
+            class_index (int): Índice da classe atual.
+
+        Returns:
+            z3.ExprRef: Fórmula representando todos os caminhos de todas as árvores.
+        """
+        df = model.get_booster().trees_to_dataframe()
+        class_index = 0  # if model.n_classes_ == 2:
+
+        all_tree_formulas = []
+
+        for tree_index in df["Tree"].unique():
+            tree_df = df[df["Tree"] == tree_index]
+            o = Real(f'o_{tree_index}_{class_index}')
+
+            if len(tree_df) == 1 and tree_df.iloc[0]["Feature"] == "Leaf":
+                leaf_value = tree_df.iloc[0]["Gain"]
+                all_tree_formulas.append(And(o == leaf_value))
+                continue
+
+            path_formulas = []
+
+            def get_conditions(node_id):
+                conditions = []
+                current_node = tree_df[tree_df["ID"] == node_id]
+                if current_node.empty:
+                    return conditions
+
+                parent_node = tree_df[(tree_df["Yes"] == node_id) | (
+                    tree_df["No"] == node_id)]
+                if not parent_node.empty:
+                    parent_data = parent_node.iloc[0]
+                    feature = parent_data["Feature"]
+                    split_value = parent_data["Split"]
+                    x = Real(feature)
+                    if parent_data["Yes"] == node_id:
+                        conditions.append(x < split_value)
+                    else:
+                        conditions.append(x >= split_value)
+                    conditions = get_conditions(parent_data["ID"]) + conditions
+
+                return conditions
+
+            for _, node in tree_df[tree_df["Feature"] == "Leaf"].iterrows():
+                leaf_value = node["Gain"]
+                leaf_id = node["ID"]
+                conditions = get_conditions(leaf_id)
+                path_formula = And(*conditions)
+                implication = Implies(path_formula, o == leaf_value)
+                path_formulas.append(implication)
+
+            all_tree_formulas.append(And(*path_formulas))
+
+        return And(*all_tree_formulas)
 
     def tree_paths_expression(self, tree, tree_index, class_index):
         tree_ = tree.tree_
@@ -61,38 +127,29 @@ class Explainer:
                 x = Real(f"x{feature[node]}")
                 left_condition = x <= threshold[node]
                 right_condition = x > threshold[node]
-                traverse(tree_.children_left[node], path_conditions + [left_condition])
+                traverse(tree_.children_left[node],
+                         path_conditions + [left_condition])
                 traverse(
-                    tree_.children_right[node], path_conditions + [right_condition]
+                    tree_.children_right[node], path_conditions +
+                    [right_condition]
                 )
 
         traverse(0, [])
         return And(*paths)
 
-    def decision_function_expression(self, model, x):
-        learning_rate = model.learning_rate
-        estimators = model.estimators_
+    def decision_function_expression(self, model, x, label_proportions):
         n_classes = 1 if model.n_classes_ <= 2 else model.n_classes_
-
-        decision = model.decision_function(x)
         predicted_class = model.predict(x)[0]
-
-        estimator_results = []
-        for estimator in estimators:
-            class_predictions = [tree.predict(x) for tree in estimator]
-            estimator_results.append(class_predictions)
-
-        estimator_sum = np.sum(estimator_results, axis=0) * learning_rate
-        init_value = decision - estimator_sum.T
+        init_value = label_proportions
 
         equation_list = []
         for class_number in range(n_classes):
             estimator_list = []
-            for estimator_number in range(len(estimators)):
+            for estimator_number in range(int(len(model.get_booster().get_dump()) / n_classes)):
                 o = Real(f"o_{estimator_number}_{class_number}")
                 estimator_list.append(o)
             equation_o = (
-                Sum(estimator_list) * learning_rate + init_value[0][class_number]
+                Sum(estimator_list) + init_value
             )
             equation_list.append(equation_o)
 
@@ -113,7 +170,8 @@ class Explainer:
         return And(final_equation)
 
     def instance_expression(self, instance):
-        formula = [Real(f"x{i}") == value for i, value in enumerate(instance)]
+        formula = [Real(self.columns[i]) == value for i,
+                   value in enumerate(instance)]
         return formula
 
     def explain_expression(self, I, T, D, model, reorder):
@@ -123,14 +181,16 @@ class Explainer:
 
         importances = model.feature_importances_
         non_zero_indices = np.where(importances != 0)[0]
+        print(non_zero_indices)
 
         if reorder == "asc":
-            sorted_feature_indices = np.argsort(non_zero_indices)
+            sorted_feature_indices = non_zero_indices[np.argsort(importances[non_zero_indices])]
             X = [X[i] for i in sorted_feature_indices]
         elif reorder == "desc":
-            sorted_feature_indices = np.argsort(np.flip(non_zero_indices))
+            sorted_feature_indices = non_zero_indices[np.argsort(-importances[non_zero_indices])]
             X = [X[i] for i in sorted_feature_indices]
 
+        print(sorted_feature_indices)
         for feature in X.copy():
             X.remove(feature)
 

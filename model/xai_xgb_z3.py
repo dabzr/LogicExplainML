fb83bee2c138b17dd9993b32941528f211e637d9
vinicius <viniciuspxc@gmail.com>
2025-01-13 16:23:08 -0300

    binary feature constraints

--- model/xai_xgb_z3.py
+++ model/xai_xgb_z3.py
@@ -24,23 +24,35 @@ class XGBoostExplainer:
 
     def explain(self, instance, reorder="asc"):
         self.D = self.decision_function_expression(
-            self.model, [instance], self.label_proportions)
+            self.model, [instance], self.label_proportions
+        )
         self.I = self.instance_expression(instance)
 
         return self.explain_expression(self.I, self.T, self.D, self.model, reorder)
 
-    def feature_constraints_expression(self, X):
+    def feature_constraints_expression(self, X: np.ndarray):
+        """
+        Recebe um dataset e retorna uma fórmula no z3 com:
+        - Restrições de valor máximo e mínimo para features contínuas.
+        - Restrições de igualdade para features categóricas binárias.
+        """
         constraints = []
 
         for i in range(X.shape[1]):
             feature_values = X[:, i]
-            min_val, max_val = feature_values.min(), feature_values.max()
+            unique_values = np.unique(feature_values)
 
             x = Real(self.columns[i])
-            min = RealVal(min_val)
-            max = RealVal(max_val)
 
-            constraint = And(min <= x, x <= max)
+            if len(unique_values) == 2:
+                a, b = unique_values
+                constraint = Or(x == RealVal(a), x == RealVal(b))
+            else:
+                min_val, max_val = feature_values.min(), feature_values.max()
+                min_val_z3 = RealVal(min_val)
+                max_val_z3 = RealVal(max_val)
+                constraint = And(min_val_z3 <= x, x <= max_val_z3)
+
             constraints.append(constraint)
 
         return And(*constraints)
@@ -64,7 +76,7 @@ class XGBoostExplainer:
 
         for tree_index in df["Tree"].unique():
             tree_df = df[df["Tree"] == tree_index]
-            o = Real(f'o_{tree_index}_{class_index}')
+            o = Real(f"o_{tree_index}_{class_index}")
 
             if len(tree_df) == 1 and tree_df.iloc[0]["Feature"] == "Leaf":
                 leaf_value = tree_df.iloc[0]["Gain"]
@@ -79,8 +91,9 @@ class XGBoostExplainer:
                 if current_node.empty:
                     return conditions
 
-                parent_node = tree_df[(tree_df["Yes"] == node_id) | (
-                    tree_df["No"] == node_id)]
+                parent_node = tree_df[
+                    (tree_df["Yes"] == node_id) | (tree_df["No"] == node_id)
+                ]
                 if not parent_node.empty:
                     parent_data = parent_node.iloc[0]
                     feature = parent_data["Feature"]
@@ -106,37 +119,6 @@ class XGBoostExplainer:
 
         return And(*all_tree_formulas)
 
-    def tree_paths_expression(self, tree, tree_index, class_index):
-        tree_ = tree.tree_
-        feature = tree_.feature
-        threshold = tree_.threshold
-        value = tree_.value
-
-        paths = []
-        o = Real(f"o_{tree_index}_{class_index}")
-
-        def traverse(node, path_conditions):
-
-            if feature[node] == -2:
-                leaf_value = value[node][0][0]
-                path_formula = And(path_conditions)
-                implication = Implies(path_formula, o == leaf_value)
-                paths.append(implication)
-            else:
-
-                x = Real(f"x{feature[node]}")
-                left_condition = x <= threshold[node]
-                right_condition = x > threshold[node]
-                traverse(tree_.children_left[node],
-                         path_conditions + [left_condition])
-                traverse(
-                    tree_.children_right[node], path_conditions +
-                    [right_condition]
-                )
-
-        traverse(0, [])
-        return And(*paths)
-
     def decision_function_expression(self, model, x, label_proportions):
         n_classes = 1 if model.n_classes_ <= 2 else model.n_classes_
         predicted_class = model.predict(x)[0]
@@ -145,12 +127,12 @@ class XGBoostExplainer:
         equation_list = []
         for class_number in range(n_classes):
             estimator_list = []
-            for estimator_number in range(int(len(model.get_booster().get_dump()) / n_classes)):
+            for estimator_number in range(
+                int(len(model.get_booster().get_dump()) / n_classes)
+            ):
                 o = Real(f"o_{estimator_number}_{class_number}")
                 estimator_list.append(o)
-            equation_o = (
-                Sum(estimator_list) + init_value
-            )
+            equation_o = Sum(estimator_list) + init_value
             equation_list.append(equation_o)
 
         if n_classes <= 2:
@@ -170,8 +152,7 @@ class XGBoostExplainer:
         return And(final_equation)
 
     def instance_expression(self, instance):
-        formula = [Real(self.columns[i]) == value for i,
-                   value in enumerate(instance)]
+        formula = [Real(self.columns[i]) == value for i, value in enumerate(instance)]
         return formula
 
     def explain_expression(self, I, T, D, model, reorder):
@@ -183,10 +164,14 @@ class XGBoostExplainer:
         non_zero_indices = np.where(importances != 0)[0]
 
         if reorder == "asc":
-            sorted_feature_indices = non_zero_indices[np.argsort(importances[non_zero_indices])]
+            sorted_feature_indices = non_zero_indices[
+                np.argsort(importances[non_zero_indices])
+            ]
             X = [X[i] for i in sorted_feature_indices]
         elif reorder == "desc":
-            sorted_feature_indices = non_zero_indices[np.argsort(-importances[non_zero_indices])]
+            sorted_feature_indices = non_zero_indices[
+                np.argsort(-importances[non_zero_indices])
+            ]
             X = [X[i] for i in sorted_feature_indices]
 
         for feature in X.copy():
